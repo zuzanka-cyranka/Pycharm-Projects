71f26c4e7ec9b2ab4c5d2ea1e4d56686a23826b4
Zuzanka <zuzanna.zahorska@gmail.com>
2020-05-11 14:01:17 +0200

    blah

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,7 +1,8 @@
+i
 def fizzbuzz(number:int):
     count = 0
     while count < number:
-        count +=1
+        count += 1
         if count % 3 == 0 and count % 5 == 0:
             print("FizzBuzz")
         elif count % 3 == 0:
@@ -10,5 +11,4 @@ def fizzbuzz(number:int):
             print("Buzz")
         else:
             print(count)
-
-fizzbuzz()
+fizzbuzz(15)
---
+++ encrypted_message_once_again.py
@@ -0,0 +1,21 @@
+# Define an encrypt_message function that accepts a string.
+# The input string will consist of only alphabetic characters.
+# The function should return a string where all characters have been moved
+# "up" two spots in the alphabet. For example, "a" will become "c".
+#
+#
+# encrypt_message("abc") => "cde"
+# encrypt_message("xyz") => "zab"
+# encrypt_message("")    => "
+
+def encrypt_message(input_string:str):
+    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
+                "v", "w", "x", "y", "z"]
+    input_string_index = []
+    result = []
+
+
+    return input_string_index
+
+
+print(encrypt_message("zaba")) #bcdc
